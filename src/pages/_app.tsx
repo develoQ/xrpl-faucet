b75f7e967625f14fe588d2a9b19de10729652e8a
develoQ <develoQ.jp@gmail.com>
2022-10-01 12:43:47 +0900

    fix metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,14 +11,14 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
     <>
       <Head>
         <title>XRP Ledger Faucet</title>
-        <meta name='description' content='Provides XRP to XRP Ledger&#39;s testnet address.' />
+        <meta name='description' content='Provides XRP to XRP Ledger testnet account.' />
         <link rel='icon' href='/favicon.svg' />
         <meta name='viewport' content='width=device-width,initial-scale=1.0' />
         <meta property='og:type' content='website' />
         <meta property='og:url' content='https://faucet.tequ.dev' />
-        <meta property='og:title' content='XRP Easy NFT' />
+        <meta property='og:title' content='XRP Ledger Faucet' />
         <meta property='og:image' content='https://faucet.tequ.dev/ogp.png' />
-        <meta name='og:description' content='Provides XRP to XRP Ledger&#39;s testnet address.' />
+        <meta name='og:description' content='Provides XRP to XRP Ledger testnet account.' />
         <meta name='twitter:card' content='summary_large_image' />
         <meta name='twitter:creator' content='@_tequ_' />
       </Head>
